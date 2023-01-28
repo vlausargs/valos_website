efcb40959de9d5705957e203858b91411c77cca1
vivaldyaugusta <vivaldy.augusta@qualitas.co.id>
2023-01-29 05:33:09 +0700

    add post to page blog

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,8 +1,13 @@
+import { useRouter } from "next/router";
 
 type Props = {
     children: React.ReactNode,
 };
 const Layout: React.FC<Props> = ({ children }): JSX.Element => {
+    const router = useRouter();
+    const handleRoute = async(route:string)=> {
+        await router.push(route);
+    }
     return (
         <div className="font-mono flex flex-col min-w-full min-h-screen bg-gray-900">
 
@@ -14,13 +19,13 @@ const Layout: React.FC<Props> = ({ children }): JSX.Element => {
             </div>
             <div className="flex flex-col w-full">
                 <div className="flex flex-row mt-6 ml-20 text-gray-400 text-2xl font-bold ">
-                    <div className="flex flex-col hover:text-white"> Home </div>
+                    <button className="flex flex-col hover:text-white" onClick={(e)=>void handleRoute('/home')}> Home </button>
                     <div className="flex flex-col mx-2">/ </div>
-                    <div className="flex flex-col hover:text-white"> Blog </div>
+                    <button className="flex flex-col hover:text-white" onClick={(e)=>void handleRoute('/blog')}> Blog </button>
                     <div className="flex flex-col mx-2">/ </div>
-                    <div className="flex flex-col hover:text-white"> Who is Valos </div>
+                    <button className="flex flex-col hover:text-white" onClick={(e)=>void handleRoute('/whois')}> Who is Valos </button>
                     <div className="flex flex-col mx-2">/ </div>
-                    <div className="flex flex-col hover:text-white"> Contact </div>
+                    <button className="flex flex-col hover:text-white" onClick={(e)=>void handleRoute('/contact')}> Contact </button>
                 </div>
                 <div className="flex flex-col w-full">
                     {children}
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,12 +1,35 @@
 import { type NextPage } from "next";
+import { api } from "../../utils/api";
+import parse from 'html-react-parser';
 
+const PostEntries: React.FC = () => {
+    const { data: postEntries, isLoading } = api.post.getAll.useQuery();
+
+    if (isLoading) {
+        return <div>Fetching messages...</div>;
+    }
+
+    return (
+        <div className="flex flex-col gap-4">
+            {postEntries?.map((entry, index) => (
+                <div key={index} className="flex flex-col">
+                    <div className="flex flex-col">{parse(entry?.content?entry?.content:"")}</div>
+                    <div className="flex flex-row">
+                        {entry.tag.map((obj, idx) => (<div key={idx} className="text-center rounded-md border-2 border-zinc-800 p-1 m-2">{obj.desc}</div>))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
 const Blog: NextPage = () => {
     return (
         <div className="flex lg:flex-row flex-col w-full px-10 lg:p-0">
             <div className="flex flex-col bg-white w-full lg:w-3/4 min-h-[40rem] mt-10 lg:ml-20 lg:mr-4 p-10">
                 <div>
-                    Titles
+                    Blog Content
                 </div>
+                <PostEntries />
             </div>
             <div className="flex flex-col bg-white w-full lg:w-1/4 min-h-[40rem] mt-10 lg:ml-4 lg:mr-20 p-10">
                 <div className="flex flex-col">side bar</div>
